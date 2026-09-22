6db73fc7e445af265dbd70ea7c67ce1aa1f1a1ec
varunmithra16 <vnatukula@csod.com>
2026-09-22 22:49:48 +0530

    Initializing FinTrack Project Backend

---
+++ src/main/java/com/fintrack/fintrack_backend/FintrackBackendApplication.java
@@ -0,0 +1,14 @@
+package com.fintrack.fintrack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintrackBackendApplication.class, args);
+		System.out.println("Fintrack Backend Application started successfully.");
+	}
+
+}
